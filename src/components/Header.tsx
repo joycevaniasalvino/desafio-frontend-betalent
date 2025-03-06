11889c9003370aa8bb24a53c658c880deb68a7eb
Joycevânia Salvino <joycelima773@gmail.com>
2025-03-06 19:33:03 -0300

    :truck: chore: organizando pastas

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import Logo from "../assets/images/Logo.png"
+function Header(){
+    return (
+        <header>
+            <img src={Logo} alt="Logo BeTalent" />
+        </header>
+    )
+}
+
+export default Header;
