f653dd9ff2149b61b7752c63e837431b4431bd38
Joycevânia Salvino <joycelima773@gmail.com>
2025-03-07 00:44:42 -0300

    :wrench: chore: subindo alterações

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,10 +1,6 @@
 export function formatDate(dateString: string): string {
     const date = new Date(dateString);
-<<<<<<< HEAD
     if (isNaN(date.getTime())) return "Data inválida";
-=======
-    if (isNaN(date.getTime())) return "Data inválida"; // Evita erro caso o formato esteja errado
->>>>>>> dad962a6537c25adf96b8e0aa67889a03f790582
 
     return date.toLocaleDateString("pt-BR", {
         day: "2-digit",
