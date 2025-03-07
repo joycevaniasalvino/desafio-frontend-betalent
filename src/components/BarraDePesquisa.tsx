2c757f1b3f66e3afdeb21ac0d210321dda533ce8
Joycevânia Salvino <joycelima773@gmail.com>
2025-03-06 21:05:47 -0300

    :art: style: estilização da barra de pesquisa

--- src/components/BarraDePesquisa.tsx
+++ src/components/BarraDePesquisa.tsx
@@ -1,7 +1,11 @@
-function BarraDePesquisa(){
-    return(
+function BarraDePesquisa() {
+    return (
         <div className="barraPesquisa">
-            <input type="text" />
+            <input type="text" placeholder="Pesquisar" />
+            <svg className="svg-pesquisa" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <title />
+                <g data-name="Layer 2" id="Layer_2"><path d="M18,10a8,8,0,1,0-3.1,6.31l6.4,6.4,1.41-1.41-6.4-6.4A8,8,0,0,0,18,10Zm-8,6a6,6,0,1,1,6-6A6,6,0,0,1,10,16Z" /></g>
+            </svg>
         </div>
     )
 }
