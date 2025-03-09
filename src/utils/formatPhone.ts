44b83b5bf2653e167c00e83b46888f3206312ccf
Joycevânia Salvino <joycelima773@gmail.com>
2025-03-08 21:36:07 -0300

    :wrench: fix: correção da função de formatar telefone e barra de pesquisa

--- src/utils/formatPhone.ts
+++ src/utils/formatPhone.ts
@@ -3,5 +3,5 @@ export function formatPhone(phone: string): string {
 
     if (cleaned.length !== 13) return "Número inválido";
 
-    return `+${cleaned.slice(0, 2)} ${cleaned.slice(2, 4)} ${cleaned[4]} ${cleaned.slice(5, 9)}-${cleaned.slice(9)}`;
+    return `+${cleaned.slice(0, 2)} (${cleaned.slice(2, 4)}) ${cleaned[4]} ${cleaned.slice(5, 9)}-${cleaned.slice(9)}`;
 }
